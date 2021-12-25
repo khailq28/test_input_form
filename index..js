b3677f11504cb19684e323f737ca46109df0972e
khailq28 <67352490+khailq28@users.noreply.github.com>
2021-12-25 13:41:19 +0700

    Update index..js

--- index..js
+++ index..js
@@ -8,3 +8,11 @@ document.querySelector('#user_password_confirmation').value = '';
 /*
 ="document.querySelector('#user_first_name').value = '"&B3&"';document.querySelector('#user_last_name').value = '"&C3&"';document.querySelector('#user_email').value = '"&F3&"';document.querySelector('#user_password').value = '"&G3&"';document.querySelector('#user_password_confirmation').value = '"&G3&"';"
 */
+
+document.querySelector('#entities_person_first_name').value = '';document.querySelector('#entities_person_middle_name').value = '';document.querySelector('#entities_person_last_name').value = '';
+
+// excel
+/*
+="document.querySelector('#entities_person_first_name').value = '"&B3&"';document.querySelector('#entities_person_middle_name').value = '"&B3&"';document.querySelector('#entities_person_last_name').value = '"&B3&"';
+"
+*/
