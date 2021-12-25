feb7eb9003bef7f64c839e84a5d9c62998fe650b
khailq28 <67352490+khailq28@users.noreply.github.com>
2021-12-25 12:27:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+document.querySelector('#user_first_name').value = '';
+document.querySelector('#user_last_name').value = '';
+document.querySelector('#user_email').value = '';
+document.querySelector('#user_password').value = '';
+document.querySelector('#user_password_confirmation').value = '';
 
+// excel
+document.querySelector('#user_first_name').value = 'haha';document.querySelector('#user_last_name').value = 'hihi';document.querySelector('#user_email').value = 'khailuong61@gmail.com';document.querySelector('#user_password').value = 'khailq281298';document.querySelector('#user_password_confirmation').value = 'khailq281298';
